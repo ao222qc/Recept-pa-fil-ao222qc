3b1ae82c5d472b066f28447c58026ef4a0d0bf4e
ao222qc <ao222qc@student.lnu.se>
2014-10-22 02:34:12 -0700

    Load working, part of recipeview working

---
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -0,0 +1,40 @@
+﻿using FiledRecipes.Domain;
+using FiledRecipes.App.Mvp;
+using FiledRecipes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiledRecipes.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RecipeView : ViewBase, IRecipeView
+    {
+        public void Show(IRecipe recipe)
+        {
+            Header = recipe.Name;
+            ShowHeaderPanel();
+            //Console.WriteLine(recipe.Name);
+
+            foreach (Ingredient ingredienser in recipe.Ingredients)
+            {
+                Console.WriteLine(ingredienser);
+            }
+            foreach (string instruction in recipe.Instructions)
+            {
+                Console.WriteLine(instruction);
+            }
+          
+        }
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+ 
+        }
+   
+
+
+
+    }
+}
